c00c88ef20368e9373b860eca23b8fde5db2fe95
Priya Tripathi <p.tripathi.ibm@gmail.com>
2020-05-23 20:10:20 +0530

    Initial Commit

---
+++ Day1-DataTypes.py
@@ -0,0 +1,11 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+secondInt = int(input())
+secondDouble = float(input())
+secondString = input()
+
+print(i+secondInt)
+print(d+secondDouble)
+print(s+secondString)
