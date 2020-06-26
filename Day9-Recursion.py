6f790a16f4d20d040b683bceb28aac1a82ca829c
Priya Tripathi <p.tripathi.ibm@gmail.com>
2020-06-26 11:40:23 +0530

    consecutive commit

---
+++ Day9-Recursion.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the factorial function below.
+def factorial(n):
+    if n==1:
+        return 1
+    else:
+        if n>1:
+            result = n * factorial(n-1)
+            return result
+
+if __name__ == '__main__':
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    fptr = sys.stdout
+    n = int(input())
+
+    result = factorial(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
